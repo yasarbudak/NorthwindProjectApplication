3f17310f6ab29d370cb49c2419aebf841d974d92
yasarbudak <yasar.budak@hotmail.com>
2021-11-05 20:53:23 +0300

    WebAPI project added.

---
+++ WebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,52 @@
+﻿using Business.Abstract;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+  [Route("api/[controller]")]
+  [ApiController]//Attribute //For java Annonation
+  public class ProductsController : ControllerBase
+  {
+    //Loosely coupled
+    IProductService _productService;
+
+    public ProductsController(IProductService productService)
+    {
+      _productService = productService;
+    }
+
+    [HttpGet("getall")]
+    public IActionResult GetAll()
+    {
+      //Swagger
+      //Dependency chain!
+      //IProductService productService = new ProductManager(new EFProductDal());
+      var result = _productService.GetAll();
+      if (result.Success)
+        return Ok(result);
+      else
+        return BadRequest(result);
+    }
+
+    [HttpGet("getbyid")]
+    public IActionResult GetById(int productId)
+    {
+      var result = _productService.GetById(productId);
+      if (result.Success)
+        return Ok(result);
+      else
+        return BadRequest(result);
+    }
+
+    [HttpPost("add")]
+    public IActionResult Add(Product product)
+    {
+      var result = _productService.Add(product);
+      if (result.Success)
+        return Ok(result);
+      else
+        return BadRequest(result);
+    }
+  }
+}
