68612255577e292b1a226ba04975cd5a9075c78c
yasarbudak <yasar.budak@hotmail.com>
2021-12-16 18:34:25 +0300

    Implemented caching, performance and transaction operations.

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -2,6 +2,9 @@
 using Business.BusinessAspects.Autofac;
 using Business.Constants;
 using Business.ValidationRules.FluentValidation;
+using Core.Aspects.Autofac.Caching;
+using Core.Aspects.Autofac.Performance;
+using Core.Aspects.Autofac.Transaction;
 using Core.Aspects.Autofac.Validation;
 using Core.Utilities.Business;
 using Core.Utilities.Results;
@@ -29,6 +32,7 @@ namespace Business.Concrete
     //rainbow table
     [SecuredOperation("product.add, admin, editor")]
     [ValidationAspect(typeof(ProductValidator))]
+    [CacheRemoveAspect("IProductService.Get")]
     public IResult Add(Product product)
     {
       IResult result = BusinessRules.Run(CheckIfProductCountOfCategoryCorrect(product.ProductId), CheckIfProductNameExist(product.ProductName), CheckIfCategoryLimitExceeded());
@@ -70,12 +74,15 @@ namespace Business.Concrete
       return new SuccessDataResult<List<ProductDetailDTO>>(_productDal.GetProductDetails());
     }
 
+    [CacheAspect]
+    [PerformanceAspect(5)]
     public IDataResult<Product> GetById(int productId)
     {
       return new SuccessDataResult<Product>(_productDal.Get(p => p.ProductId == productId));
     }
 
     [ValidationAspect(typeof(ProductValidator))]
+    [CacheRemoveAspect("IProductService.Get")]
     public IResult Update(Product product)
     {
       var result = _productDal.GetAll(p => p.CategoryId == product.CategoryId).Count;
@@ -116,6 +123,12 @@ namespace Business.Concrete
         return new ErrorResult(Messages.CategoryLimitExceeded);
       }
       return new SuccessResult();
+    }
+
+    [TransactionScopeAspect]
+    public IResult AddTransactionalTest(Product product)
+    {
+      throw new NotImplementedException();
     }
   }
 }
